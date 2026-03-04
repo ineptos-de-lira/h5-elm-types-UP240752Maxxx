bb7cdcf860f5445eed08e7133d1c42b17ae3d206
Max Esquivel <up240752@alumnos.upa.edu.mx>
2026-03-04 14:20:56 -0600

    first commit

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,47 @@
 module Helper exposing (..)
+
+
+type GradeStatus
+    = Approved
+    | Failed
+    | Pending
+
+
+categoricalGrade : List Float -> List GradeStatus
+categoricalGrade grades =
+    List.map toStatus grades
+
+
+toStatus : Float -> GradeStatus
+toStatus grade =
+    if grade < 0 then
+        Pending
+
+    else if grade >= 7 then
+        Approved
+
+    else
+        Failed
+
+
+type AirplaneStatus
+    = Canceled
+    | Delayed
+    | OnTime
+    | Boarding
+
+
+airplaneScheduleAction : AirplaneStatus -> String
+airplaneScheduleAction status =
+    case status of
+        Canceled ->
+            "Pedir reembolso"
+
+        Delayed ->
+            "Esperar"
+
+        OnTime ->
+            "Esperar"
+
+        Boarding ->
+            "Abordar"
